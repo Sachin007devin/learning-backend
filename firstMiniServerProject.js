5a4c439e84a2ba70eaec488cd960f313ca3bb79d
Sachin007devin <rustystrtdx@gmail.com>
2026-08-10 13:54:25 +0530

    first mini project to apply middleware and routing

---
+++ firstMiniServerProject.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app =  express()
+
+app.use((req,res,next)=>{
+    const requestType = req.method
+    const endpoint = req.url
+    console.log(`${requestType} request made to ${endpoint}`)
+    next()
+})
+
+app.get('/products',(req,res)=>{
+    res.send('<h1>Here is the list of all products</h1>')
+})
+
+app.get('/categories',(req,res)=>{
+    res.send('<h1>Here is the list of all categories</h1>')
+})
+
+app.post('/products',(req,res)=>{
+    res.send('<h1>A new product has been added</h1>')
+})
+
+app.post('/categories',(req,res)=>{
+    res.send('<h1>A new category has been created</h1>')
+})
+
+app.listen(4000,()=>{
+    console.log('Server is running on http://localhost:4000')
+})
